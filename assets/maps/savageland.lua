5c6b6aa3c48d3c2f5103970839129d55720c0bf8
AlexFolland <lexlexlex@gmail.com>
2016-06-28 03:30:01 -0700

    add cool layers to savage land

--- assets/maps/savageland.lua
+++ assets/maps/savageland.lua
@@ -28,12 +28,22 @@ return {
       properties = {},
       terrains = {
         {
-          name = "gravel",
+          name = "2 - gravel",
           tile = -1,
           properties = {}
         },
         {
-          name = "gravel 2",
+          name = "2 - road",
+          tile = -1,
+          properties = {}
+        },
+        {
+          name = "2 - grassness",
+          tile = -1,
+          properties = {}
+        },
+        {
+          name = "3 - stone brick",
           tile = -1,
           properties = {}
         }
@@ -77,12 +87,88 @@ return {
           terrain = { 1, -1, -1, -1 }
         },
         {
+          id = 46,
+          terrain = { -1, 3, 3, 3 }
+        },
+        {
+          id = 47,
+          terrain = { 3, -1, 3, 3 }
+        },
+        {
+          id = 58,
+          terrain = { -1, 1, 1, -1 }
+        },
+        {
+          id = 59,
+          terrain = { -1, -1, -1, 3 }
+        },
+        {
+          id = 60,
+          terrain = { -1, -1, 3, 3 }
+        },
+        {
+          id = 61,
+          terrain = { -1, -1, 3, -1 }
+        },
+        {
+          id = 62,
+          terrain = { 3, 3, -1, 3 }
+        },
+        {
+          id = 63,
+          terrain = { 3, 3, 3, -1 }
+        },
+        {
+          id = 74,
+          terrain = { 1, -1, -1, 1 }
+        },
+        {
+          id = 75,
+          terrain = { -1, 3, -1, 3 }
+        },
+        {
+          id = 76,
+          terrain = { 3, 3, 3, 3 }
+        },
+        {
+          id = 77,
+          terrain = { 3, -1, 3, -1 }
+        },
+        {
+          id = 90,
+          terrain = { -1, 2, 2, -1 }
+        },
+        {
+          id = 91,
+          terrain = { -1, 3, -1, -1 }
+        },
+        {
+          id = 92,
+          terrain = { 3, 3, -1, -1 }
+        },
+        {
+          id = 93,
+          terrain = { 3, -1, -1, -1 }
+        },
+        {
+          id = 106,
+          terrain = { 2, -1, -1, 2 }
+        },
+        {
           id = 110,
           terrain = { -1, 1, 1, 1 }
         },
         {
           id = 111,
           terrain = { 1, -1, 1, 1 }
+        },
+        {
+          id = 118,
+          terrain = { -1, 2, 2, 2 }
+        },
+        {
+          id = 119,
+          terrain = { 2, -1, 2, 2 }
         },
         {
           id = 126,
@@ -93,12 +179,32 @@ return {
           terrain = { 1, 1, 1, -1 }
         },
         {
+          id = 134,
+          terrain = { 2, 2, -1, 2 }
+        },
+        {
+          id = 135,
+          terrain = { 2, 2, 2, -1 }
+        },
+        {
           id = 142,
           terrain = { -1, 0, 0, 0 }
         },
         {
           id = 143,
           terrain = { 0, -1, 0, 0 }
+        },
+        {
+          id = 148,
+          terrain = { -1, -1, -1, 2 }
+        },
+        {
+          id = 149,
+          terrain = { -1, -1, 2, 2 }
+        },
+        {
+          id = 150,
+          terrain = { -1, -1, 2, -1 }
         },
         {
           id = 155,
@@ -121,6 +227,18 @@ return {
           terrain = { 0, 0, 0, -1 }
         },
         {
+          id = 164,
+          terrain = { -1, 2, -1, 2 }
+        },
+        {
+          id = 165,
+          terrain = { 2, 2, 2, 2 }
+        },
+        {
+          id = 166,
+          terrain = { 2, -1, 2, -1 }
+        },
+        {
           id = 171,
           terrain = { -1, 0, -1, 0 }
         },
@@ -131,6 +249,18 @@ return {
         {
           id = 173,
           terrain = { 0, -1, 0, -1 }
+        },
+        {
+          id = 180,
+          terrain = { -1, 2, -1, -1 }
+        },
+        {
+          id = 181,
+          terrain = { 2, 2, -1, -1 }
+        },
+        {
+          id = 182,
+          terrain = { 2, -1, -1, -1 }
         },
         {
           id = 187,
@@ -150,7 +280,7 @@ return {
   layers = {
     {
       type = "tilelayer",
-      name = "floor",
+      name = "level 1 floor 1",
       x = 0,
       y = 0,
       width = 240,
@@ -166,7 +296,7 @@ return {
     },
     {
       type = "tilelayer",
-      name = "obstacles",
+      name = "level 1 floor 2",
       x = 0,
       y = 0,
       width = 240,
@@ -178,7 +308,87 @@ return {
       properties = {},
       encoding = "base64",
       compression = "zlib",
-      data = "eJzt3UluLEUQBuDHuGfcM+4Z94x7xj3jDewb2DcDTgCcADgBsCUQsvQk3H7d7hryz/g+KZbuLpXaOUREZT14sK7HKh6veKLiyYqnKp5e+TuBZTxT8WzFcxXPV7xQ8eKuVwQc66WKlyteqXi14rWK10/4+4uKyxWuC3i0NyrerHir4u2KdyrePeHvryquV7gumN0Sc997Fe9XfFDxYcVHFR+f+ZmMy3ppHEvMfZ9UfFrxWcXnFV9UfHnmZ55q699U59+w9dKY7ptH/qri64pvKr6t+K7i+5Wu8ZCtf1N+w1k6jLfyyMyqw3h7bh6ZdXSYOzjfuXlk1pE8d+jt2Y48MkuzJ9vOCHlk5mJPtp0R8sjMxZ4MctmTQa6Z92TqAsxu5j1Zcl3gX2oDkEttAHKpDUAutQHIpTYAuWauDcCIlswZz1wbgBHJGUMuOWPIJWcMuTrljPWHMZtOOWN7fWbTKWdsrw+57PUhV6e9Psym014fZtNprw/QmXoj5FJvhFzqjZBLvRFyqTfen9wBe1NvvL8OuQNj1NjUG++vQ+6gwxhFTx1yBx3GKHrqkDvoMEbRU4fcQYcxip465A46jFEwqw5jFAAAAMCNi4rLvS8CuJeriuuFP9OYALnWGBMAAAAAOnKOAeRyjgHkco4B5HKOAdwuYW/pHAO4XcLe0jkGcLuEvaVzDEhYJ+7B3pIECevEPdhbkiBhnbgHe0sSWCfezt6SBNaJkMs6EXJZJwIAAMBy9HlBLv+/kEufJuTSpwm59GlCLn2akEufJuTSp8m5vBcQcs36XkC1Vciltgq51FYhl9oq5FJbhVxqq5BLbRUAAACAU+izhlyz9lnTlx57yKXHHnLpsYdceuwhlx57yKXHHnLpsQcAuJt6PeRSr4dc6vWQS70ecqnXQ67O9fofKn6s+Gnj75UzZCmd6/U/V/xS8evG3ytnSBdrzlW/Vfxe8cdCn3csOcPjOB8j35pz1Z8Vf1X8vdDnHUvO8DjOx8g341wlZ0gXM85VnXOG9DLjXNU5Z0gv5irIZa4CAAAAAAAAAO7i+SbI5fkmAEbjjB/I5YwfyDXjuQnQxYznJkAXo5yboMYDpxvl3AQ1HjidcxPYk/oH5FL/gFzqH8yqw9pS/YNZdVhbjlL/gKV1WFuOUv+ApXVYW6p/MCtrS8hlbQm5rC0BAAAAAAAAAACYQYfnsWFWHZ7Hhll1eB4bZtXheWyYleexIVf689jyb3SW/jy2/Bvkkn+DXPJvkEv+DXKl59+gs/T8GwAAcLeLisu9L2JCennYwlXF9d4XMSG9PJBLLw+zm3mNqZeH2c28xtTLw+xmXmPq5WF2M68x9fIwO2tMyGWNubyZc4KMxRpzeTPnBGF2M+cEYXYz5wRhdnKCkEtOEHLJCQIAeMYeknnGHgAAAAAAAAAAAAAAAAAA+nA+AORyPsB/vHuEU5j3xuL/9zD35v/Me2Px7qDD3BtG591Bh7k3jM67gw5zbxiddwcd5t7wsBHzId4ddJh7w8NGzId4d9BhyfdmxLkinXwIWxlxrkgnH8JWzBXLkw9hK+aK5cmHsBVzxfKS8yFkMVdALnMFAAAAAABsY6+zf5w5BOfb6+yfkc4cuhlLjCePZtxlNDdjySjjychGGnePZcxhLX5b60scc8jgtwUAAAAAAAAAAAAAwCj+AY0+Oh4="
+      data = "eJzt1rENwjAQhlEPAEwANKQEGtiLjMDoOYp0Dlg0Z0vvSV9/zS+7FAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGA0l2jKPgL4yyN6Zh8BAAAAAAAAAAAwuF20r3TIPAr46bPdV3SqNBcbhh6tb+7WdtfOWQcCVd/eXPuFvh1L23btF/rTut93dM85EdjQul9vL/THfmFc1+jWkL8zAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA2RYeYw07"
+    },
+    {
+      type = "tilelayer",
+      name = "level 1 floor 3",
+      x = 0,
+      y = 0,
+      width = 240,
+      height = 240,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "base64",
+      compression = "zlib",
+      data = "eJzt18EJhEAQBMDJ6BJQMQA1AjX/MG4PERYRVDhcFqqg//NpmokAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB4R5PSZunKngM8MKSMWaay5wA3/bb3E/oLtTnrbp+yFrwJuHbWXdsLdTj+vH3KErYXajDH1tc9egsAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADAv3wBwLMMLw=="
+    },
+    {
+      type = "tilelayer",
+      name = "level 1 floor 4",
+      x = 0,
+      y = 0,
+      width = 240,
+      height = 240,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "base64",
+      compression = "zlib",
+      data = "eJztzqEBADAIwDCunMfs/xP2wAQKBImsagQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADscKYHgJL8tNt+AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADAVg84YAEe"
+    },
+    {
+      type = "tilelayer",
+      name = "level 1 obstacles",
+      x = 0,
+      y = 0,
+      width = 240,
+      height = 240,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "base64",
+      compression = "zlib",
+      data = "eJzt3UluJFUQBuBm3DPuGffNtG+mPeOe8Qb2DeybAScATgCcANgSCFlq0VVuV1Vm5fvjfZ8UG0sup1LlN0REvrx3b11PVDxZ8VTF0xXPVDy78t8ElvFcxfMVL1S8WPFSxcubXhFwV69UvFrxWsXrFW9UvHnA719UXK5wXcDj3a94q+Ltincq3q1474Dfv6q4XuG6oLsl5r4HFe9XfFDxYcVHFR+f+JmMy3ppHEvMfZ9UfFrxWcXnFV9UfHniZx7q3N+pmb/D1ktjOjaP/FXF1xXfVHxb8V3F9ytd4z7n/k75DmeZYbyVR6arGcbbU/PIrGOGuYPTnZpHZh3Jc4fenvORR2Zp9mTnM0IemV7syc5nhDwyvdiTQS57MsjVeU+mLkB3nfdkyXWBf6kNQC61AcilNgC51AYgl9oA5OpcG4ARLZkz7lwbgBHJGUMuOWPIJWcMuWbKGesPo5uZcsb2+nQzU87YXh9y2etDrpn2+tDNTHt96GamvT7AzNQbIZd6I+RSb4Rc6o2QS73xeHIHbE298Xgz5A6MUWNTbzzeDLmDGcYo5jRD7mCGMYo5zZA7mGGMYk4z5A5mGKOY0wy5gxnGKOhqhjEKAAAA4MZFxeXWFwEc5arieuHPNCZArjXGBAAAAIAZOccAcjnHAHI5xwByOccAdkvYWzrHAHZL2Fs6xwB2S9hbOseAhHXiFuwtSZCwTtyCvSUJEtaJW7C3JIF14m72liSwToRc1omQyzoRAAAAlqPPC3L5/4Vc+jQhlz5NyKVPE3Lp04Rc+jQhlz5NTuW9gJCr63sB1VYhl9oq5FJbhVxqq5BLbRVyqa1CLrVVAAAAAA6hzxpyde2zZl567CGXHnvIpccecumxh1x67CGXHnvIpcceAOB26vWQS70ecqnXQy71esilXg+5Zq7X/1DxY8VPW18IHGnmev3PFb9U/Lr1hUBTa9a3fqv4veKPhT6PZTkfI9+a9a0/K/6q+Huhz7ur+zt+Jmf4KOdj5OtY33qw42dyhnTUsb61K2d4CPM3KbrWt/6fMzyE+ZsUM9e39jl1/oZzmbm+dZtT5m8AAAAAAAAA6MTzTZDL800AjMa54JDLueCQq+O5CTCLjucmwCxGOTdBjQcON8q5CWo8cDjnJrAl9Q/Ipf4BudQ/6GqGtaX6B13NsLYcpf4BS5thbTlK/QOWNsPaUv2DrqwtIZe1JeSytgQAAAAAAAAAAKCDGZ7Hhq5meB4buprheWzoaobnsaErz2NDrvTnseXfmFn689jyb5BL/g1yyb9BLvk3yJWef4OZpeffAACA211UXG59EQ3p5eEcriqut76IhvTyQC69PHTXeY2pl4fuOq8x9fLQXec1pl4euuu8xtTLQ3fWmJDLGnN5nXOCjMUac3mdc4LQXeecIHTXOScI3ckJQi45QcglJwgA4Bl7SOYZewAAAAAAAAAAAAAAAAAAmIfzASCX8wH+490jHMK8Nxb/v/u5N48y743Fu4P2c28YnXcH7efeMDrvDtrPvWF03h20n3vDw0bMh3h30H7uDQ8bMR/i3UH7Jd+bEeeKdPIhnMuIc0U6+RDOxVyxPPkQzsVcsTz5EM7FXLG85HwIWcwVkMtcAQAAAAAA57HV2T/OHILTbXX2z0hnDt2MJcaTxzPuMpqbsWSU8WRkI427d2XMYS2+W+tLHHPI4LsFAAAAAAAAAAAAAMAo/gEnMDnh"
+    },
+    {
+      type = "tilelayer",
+      name = "level 2 floor",
+      x = 0,
+      y = 0,
+      width = 240,
+      height = 240,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "base64",
+      compression = "zlib",
+      data = "eJzt1ckNg1AQREFiYEkATAI2JMCSf0ruFD4cvhBV0juONLduGgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA4I3a1KU+DZV/AcqMaUqfNFf+BSjzTb+0pPXCvf2Gera0pyOdF+7tNzzX3f0G6rm73wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAvNcfH2cFcQ=="
+    },
+    {
+      type = "tilelayer",
+      name = "level 2 obstacles",
+      x = 0,
+      y = 0,
+      width = 240,
+      height = 240,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "base64",
+      compression = "zlib",
+      data = "eJzt1TEKwCAQBEBJzAPSpLfx/0/MPcDCSFCEGdjmimO7TQkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAYNwROSM5ci3uAvS7G7dnegtgRGnc6scf9hv2ZL9hX3/sNwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADzvMF5APk="
     }
   }
 }
