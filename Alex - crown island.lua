1dde4f14b7b523f27e83206136bd3f9861796eaa
AlexFolland <lexlexlex@gmail.com>
2016-06-27 23:27:20 -0700

    add tiled map and sti as submodule

---
+++ Alex - crown island.lua
@@ -0,0 +1,69 @@
+return {
+  version = "1.1",
+  luaversion = "5.1",
+  tiledversion = "0.16.1",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 100,
+  height = 100,
+  tilewidth = 16,
+  tileheight = 16,
+  nextobjectid = 1,
+  properties = {},
+  tilesets = {
+    {
+      name = "sygma_dwtileset2",
+      firstgid = 1,
+      tilewidth = 16,
+      tileheight = 16,
+      spacing = 0,
+      margin = 0,
+      image = "../../../../tools/tiled/examples/sygma_dwtileset2.png",
+      imagewidth = 480,
+      imageheight = 256,
+      transparentcolor = "#ff799b",
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 480,
+      tiles = {}
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      name = "world map",
+      x = 0,
+      y = 0,
+      width = 100,
+      height = 100,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "base64",
+      compression = "zlib",
+      data = "eJzt1sENwiAYgNF6cxpP7uBOTtIVuqUx1QuBkprKT8k7vEtDE+gXoPM0TTMAAE3cLqvoebBaLqtSE73a+X7rrRa1XhzfI31WG5Mbx7FNap7JO/ZMmx5LRu5c06Jdl3eD+0faJB0bPe9R5fZI2iTdK3q067J1ZmkR16Uken4AAAAAAAAAAAAAcHbXHaLnOrI9HbTpt4MmfbbQQ4/RlL7ro0KPdj1qLba6RK/n7PToy6/nVenMil7PCGp3tPujvyb+r87ZJHoNI9KjX1qcgw4AAAAAAAAAAAAAAAAAAAAA/NsLbAiWXw=="
+    },
+    {
+      type = "tilelayer",
+      name = "enterable tiles",
+      x = 0,
+      y = 0,
+      width = 100,
+      height = 100,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "base64",
+      compression = "zlib",
+      data = "eJztzjEBAAAIA6AZz/6FPAyxBxKQAAAAAADA22kPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+g5eAABL"
+    }
+  }
+}
