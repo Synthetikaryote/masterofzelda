9bff14beca5b342173b89e5c7eefa3b38eb4e267
Synthetikaryote <ericfolland@gmail.com>
2016-06-29 13:09:57 -0700

    classenization

--- main.lua
+++ main.lua
@@ -1,38 +1,59 @@
+require "class"
 local sti = require "sti"
 
 -- character sprite sheet is from http://gaurav.munjal.us/Universal-LPC-Spritesheet-Character-Generator/#?sex=female&body=light&eyes=green&nose=none&ears=none&legs=none&clothes=gown&gown-underdress=1&gown-overskirt=0&gown-blue-vest=0&mail=none&armor=none&jacket=none&hair=princess_blonde&hairsara-bottomlayer=0&hairsara-shadow=0&hairsara-toplayer=0&arms=none&shoulders=none&bracers=none&greaves=none&gloves=none&hat=none&hats=tiara_purple&shoes=none&belt=none&buckle=none&necklace=none&cape=none&capeacc=none&weapon=dragonspear&ammo=none&quiver=none
 
-char = {
-    spritesheet = love.graphics.newImage("assets/character.png"),
-    animations = {
-        -- animation name = {y value, frames in animation, frames per second, xSize, ySize}
-        cast={0, 7, 20, 64, 64, 0, 32, 56},
-        thrust={1, 8, 20, 64, 64, 256, 32, 56},
-        walk={2, 8, 18, 64, 64, 512, 32, 56},
-        slash={3, 6, 20, 64, 64, 768, 32, 56},
-        shoot={4, 13, 20, 64, 64, 1024, 32, 56},
-        polearm={5, 8, 30, 192, 192, 1344, 96, 120}
-    },
-    animationQuads = {},
-    hp = 100,
-    dir = {1, 0},
-    aniDir = 4,
-    aniFrame = 0,
-    aniLastChange = 0,
-    animationName = "walk",
-    quad = love.graphics.newQuad(0, 0, 1, 1, 1, 1),
-    p = {0, 0},
-    moveSpeed = 200,
-    attackEnds = 0
-}
+Character = class(function(c, spritesheet, animations, hp, moveSpeed)
+    c.spritesheet = spritesheet
+    c.animations = animations
+    c.hp = hp
+    c.moveSpeed = moveSpeed
+    c.animationQuads = {}
+    for aniName, ani in pairs(c.animations) do
+        c.animationQuads[aniName] = {}
+        for dir = 1, 4 do
+            c.animationQuads[aniName][dir] = {}
+            for i=0, ani[2]-1 do
+                local quad = love.graphics.newQuad(i * ani[4], ani[6] + (dir - 1) * ani[5], ani[4], ani[5], c.spritesheet:getDimensions())
+                c.animationQuads[aniName][dir][i] = quad
+            end
+        end
+    end
+    c.dir = {1, 0}
+    c.aniDir = 4
+    c.aniFrame = 0
+    c.aniLastChange = 0
+    c.animationName = select(1, next(c.animations))
+    c.p = {0, 0}
+    c.attackEnds = 0
+end)
 
 local timeScale = 1
 local isPaused = false
 local font12 = love.graphics.newFont(12)
 local font72 = love.graphics.newFont(72)
 
-
 function love.load()
+    player = Character(love.graphics.newImage("assets/character.png"), {
+            -- animation name = {y value, frames in animation, frames per second, xSize, ySize}
+            cast={0, 7, 20, 64, 64, 0, 32, 56},
+            thrust={1, 8, 20, 64, 64, 256, 32, 56},
+            walk={2, 8, 18, 64, 64, 512, 32, 56},
+            slash={3, 6, 20, 64, 64, 768, 32, 56},
+            shoot={4, 13, 20, 64, 64, 1024, 32, 56},
+            polearm={5, 8, 30, 192, 192, 1344, 96, 120}
+        }, 100, 200)
+
+    orc = Character(love.graphics.newImage("assets/orc.png"), {
+            -- animation name = {y value, frames in animation, frames per second, xSize, ySize}
+            cast={0, 7, 20, 64, 64, 0, 32, 56},
+            thrust={1, 8, 20, 64, 64, 256, 32, 56},
+            walk={2, 8, 18, 64, 64, 512, 32, 56},
+            slash={3, 6, 20, 64, 64, 768, 32, 56},
+            shoot={4, 13, 20, 64, 64, 1024, 32, 56},
+            polearm={5, 8, 30, 192, 192, 1344, 96, 120}
+        }, 100, 200)
+
     map = sti.new("assets/maps/savageland.lua", { })
     for k, v in pairs(map.tiles) do
         v.sx = 5
@@ -41,10 +62,15 @@ function love.load()
 
     for k, layer in pairs(map.layers) do
         function layer:update(dt)
-            self.x = -char.p[1]
-            self.y = -char.p[2]
+            self.x = -player.p[1]
+            self.y = -player.p[2]
         end
     end
+
+    dirData = {{"up", {0, -1}},
+        {"left", {-1, 0}},
+        {"down", {0, 1}},
+        {"right", {1, 0}}}
 
     map:addCustomLayer("Sprite Layer", 6)
     local spriteLayer = map.layers["Sprite Layer"]
@@ -55,27 +81,11 @@ function love.load()
 
     -- draw callback for custom layer
     function spriteLayer:draw()
-        local animation = char.animations[char.animationName]
-        local quad = char.animationQuads[char.animationName][char.aniDir][char.aniFrame]
-        love.graphics.draw(char.spritesheet, quad, love.graphics.getWidth() * 0.5 - animation[7], love.graphics.getHeight() * 0.5 - animation[8])
+        local animation = player.animations[player.animationName]
+        local quad = player.animationQuads[player.animationName][player.aniDir][player.aniFrame]
+        love.graphics.draw(player.spritesheet, quad, love.graphics.getWidth() * 0.5 - animation[7], love.graphics.getHeight() * 0.5 - animation[8])
     end
 end
-
-for aniName, ani in pairs(char.animations) do
-    char.animationQuads[aniName] = {}
-    for dir = 1, 4 do
-        char.animationQuads[aniName][dir] = {}
-        for i=0, ani[2]-1 do
-            local quad = love.graphics.newQuad(i * ani[4], ani[6] + (dir - 1) * ani[5], ani[4], ani[5], char.spritesheet:getDimensions())
-            char.animationQuads[aniName][dir][i] = quad
-        end
-    end
-end
-
-dirData = {{"up", {0, -1}},
-        {"left", {-1, 0}},
-        {"down", {0, 1}},
-        {"right", {1, 0}}}
 
 function love.keypressed(key, scancode, isRepeat)
     if scancode == "pause" then
@@ -88,10 +98,10 @@ function love.keypressed(key, scancode, isRepeat)
     end
 
     if scancode == "space" then
-        char.animationName = "polearm"
-        local animation = char.animations[char.animationName]
+        player.animationName = "polearm"
+        local animation = player.animations[player.animationName]
         local aniDuration = animation[2] * (1 / animation[3])
-        char.attackEnds = (love.timer.getTime() + aniDuration) * timeScale
+        player.attackEnds = (love.timer.getTime() + aniDuration) * timeScale
     end
 end
 
@@ -120,29 +130,29 @@ function love.update()
     if len > 0 then
         moving = true
         -- vector direction converted to an angle and mapped to the directions in the sprite
-        char.aniDir = math.max(1, math.ceil((math.atan2(v[2], -v[1]) + 2) * 0.667 + 0.00001))
+        player.aniDir = math.max(1, math.ceil((math.atan2(v[2], -v[1]) + 2) * 0.667 + 0.00001))
         if len > 1 then v[1], v[2] = v[1] / len, v[2] / len end
-        v[1] = v[1] * char.moveSpeed * love.timer.getDelta() * timeScale * (love.keyboard.isScancodeDown("lshift") and 10 or 1)
-        v[2] = v[2] * char.moveSpeed * love.timer.getDelta() * timeScale * (love.keyboard.isScancodeDown("lshift") and 10 or 1)
-        char.p[1], char.p[2] = char.p[1] + v[1], char.p[2] + v[2]
+        v[1] = v[1] * player.moveSpeed * love.timer.getDelta() * timeScale * (love.keyboard.isScancodeDown("lshift") and 10 or 1)
+        v[2] = v[2] * player.moveSpeed * love.timer.getDelta() * timeScale * (love.keyboard.isScancodeDown("lshift") and 10 or 1)
+        player.p[1], player.p[2] = player.p[1] + v[1], player.p[2] + v[2]
     end
 
-    if love.timer.getTime() * timeScale <= char.attackEnds then
-        local animation = char.animations[char.animationName]
-        if love.timer.getTime() * timeScale - char.aniLastChange > 1 / animation[3] then
-            char.aniFrame = (char.aniFrame + 1) % animation[2]
-            char.aniLastChange = love.timer.getTime() * timeScale
+    if love.timer.getTime() * timeScale <= player.attackEnds then
+        local animation = player.animations[player.animationName]
+        if love.timer.getTime() * timeScale - player.aniLastChange > 1 / animation[3] then
+            player.aniFrame = (player.aniFrame + 1) % animation[2]
+            player.aniLastChange = love.timer.getTime() * timeScale
         end
     elseif moving then
-        char.animationName = "walk"
-        local animation = char.animations[char.animationName]
-        if love.timer.getTime() * timeScale - char.aniLastChange > 1 / animation[3] then
-            char.aniFrame = (char.aniFrame + 1) % animation[2]
-            char.aniLastChange = love.timer.getTime() * timeScale
+        player.animationName = "walk"
+        local animation = player.animations[player.animationName]
+        if love.timer.getTime() * timeScale - player.aniLastChange > 1 / animation[3] then
+            player.aniFrame = (player.aniFrame + 1) % animation[2]
+            player.aniLastChange = love.timer.getTime() * timeScale
         end
     else
-        char.animationName = "walk"
-        char.aniFrame = 0
+        player.animationName = "walk"
+        player.aniFrame = 0
     end
 
     map:update(love.timer.getDelta() * timeScale)
@@ -156,7 +166,7 @@ function love.draw()
     local joystick = love.joystick.getJoysticks()[1]
     local v = joystick and {joystick:getAxis(1), joystick:getAxis(2)} or {0, 0}
 
-    love.graphics.print("fps "..love.timer.getFPS().." x, y "..char.p[1]..", "..char.p[2]..", aniDir "..char.aniDir.." frame "..char.aniFrame..(joystick and " joystick "..joystick:getAxis(1)..", "..joystick:getAxis(2) or ""), 400, 300)
+    love.graphics.print("fps "..love.timer.getFPS().." x, y "..player.p[1]..", "..player.p[2]..", aniDir "..player.aniDir.." frame "..player.aniFrame..(joystick and " joystick "..joystick:getAxis(1)..", "..joystick:getAxis(2) or ""), 400, 300)
 
     if isPaused then
         love.graphics.setFont(font72)
@@ -165,7 +175,7 @@ function love.draw()
         love.graphics.setFont(font12)
     end
 
-    -- print_r(char.animationQuads, 800, -2600)
+    -- print_r(player.animationQuads, 800, -2600)
 end
 
 function print_r ( t, x, y )  
@@ -208,4 +218,19 @@ function print_r ( t, x, y )
     end
     love.graphics.print("", x, y)
     y = y + 20
+end
+
+function deepcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+        end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
 end
