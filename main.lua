3394875c72f351b392d62c316f5eee01e7debe4e
Alex Folland <lexlexlex@gmail.com>
2021-10-19 17:02:31 -0700

    fix for other people than just me

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ require "buildMenu"
 require "binds"
 require "server"
 
-local sti = require "sti"
+local sti = require "sti.sti"
 
 -- character sprite sheet is from http://gaurav.munjal.us/Universal-LPC-Spritesheet-Character-Generator/#?sex=female&body=light&eyes=green&nose=none&ears=none&legs=none&clothes=gown&gown-underdress=1&gown-overskirt=0&gown-blue-vest=0&mail=none&armor=none&jacket=none&hair=princess_blonde&hairsara-bottomlayer=0&hairsara-shadow=0&hairsara-toplayer=0&arms=none&shoulders=none&bracers=none&greaves=none&gloves=none&hat=none&hats=tiara_purple&shoes=none&belt=none&buckle=none&necklace=none&cape=none&capeacc=none&weapon=dragonspear&ammo=none&quiver=none
 
